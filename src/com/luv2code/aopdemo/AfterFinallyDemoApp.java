e046c3dbcda2702483c7d9a1a8593f3ee8851a96
chjinlook <chjinlook@gmail.com>
2019-07-25 16:40:11 +0900

    @After 어드바이스 추가함.  : @AfterThrowing 어드바이스보다 먼저실행됨.

--- src/com/luv2code/aopdemo/AfterFinallyDemoApp.java
+++ src/com/luv2code/aopdemo/AfterFinallyDemoApp.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 
 import com.luv2code.aopdemo.dao.AccountDAO;
 
-public class AfterThrowingDemoApp {
+public class AfterFinallyDemoApp {
 
 	public static void main(String[] args) {
 		
